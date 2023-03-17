3ed8e3dfd3d47572a832c208f87617a1705c8f73
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-03-17 22:31:49 +0530

    Signup Screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Login from "./Components/Auth/Login";
+import Welcome from "./Components/MailComponents/Welcome";
+import Singup from "./Components/Auth/Signup";
+import Mailbox from "./Components/RecivedMails/Mailbox";
+import { useSelector } from "react-redux";
+import Inbox from "./Components/RecivedMails/Inbox";
+import ReadMsg from "./Components/RecivedMails/Rec";
+import SentBox from "./Components/RecivedMails/Sent";
+import { inboxActions } from "./Components/Redux/Inbox";
+import ComposeMail from "./Components/ComposeEmail/Compose";
+import ReadSent from "./Components/RecivedMails/SentMAilsRead";
+
+
+
+import { Route,Redirect,Routes, Link} from "react-router-dom";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const isAuth=useSelector(state=>state.auth.islogin);
+  const Number=useSelector(state=>state.inbox.unRead);
+  console.log("LOHIM",isAuth)
+  return (<>  
+  
+  
+  <Routes> 
+  <Route path="/Signup" element={<Singup /> } />
+  <Route path="/" element={<Login />} />
+  <Route path="/Welcome" exact element={<Welcome />} />
+  <Route path="/Mailbox" exact element={<Mailbox />}  />
+  <Route path="Inbox" exact element={<Inbox />} />
+   <Route path='/Inbox/:id' element={ <ReadMsg /> } /> 
+   <Route path='/Sent' element={<SentBox />} />
+   <Route path='Sent/:id' element={<ReadSent  />} />
+   <Route path='/Compose' element={<ComposeMail />} />
+  
+ 
+  </Routes>
+  
+
+  </>);
 }
 
 export default App;
